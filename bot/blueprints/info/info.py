bc253744e9d3dec25de38b5936f62b826e24de74
kesha1225 <samedov03@mail.ru>
2020-01-17 00:17:09 +0300

    some useful content to info bp

--- bot/blueprints/info/info.py
+++ bot/blueprints/info/info.py
@@ -1,3 +1,5 @@
+import datetime
+
 from vk import types
 from vk.bot_framework.dispatcher import Blueprint
 from bot.rules.admin import IsAdmin
@@ -14,7 +16,9 @@ async def info(message: types.Message, data: dict):
         user: User = await User.get_user(uid=message.reply_message.from_id)
 
     result = (
-        f"uid - {user.uid}\nnickname - {user.nickname}\n"
-        f"karma - {user.karma}\n warns - {user.warns}"
+        f"uid - {user.uid}\nНик - {user.nickname}\n"
+        f"Карма - {user.karma}\nВарны - {user.warns}\n"
+        f"Время последнего сообщения -"
+        f" {datetime.datetime.fromtimestamp(user.last_message_time)}"
     )
     await message.answer(result)
