bdea72f7de4024083a543a681a14c6d50ec4e853
Thron777 <54794530+Thron777@users.noreply.github.com>
2020-01-17 13:22:47 +0500

    test
    test

--- bot/blueprints/karma/config.py
+++ bot/blueprints/karma/config.py
@@ -1,5 +1,5 @@
-karma_good = ["+", "согласен"]
+karma_good = /^\/(+|Я согласен|плюсую)/i
 karma_plus = 5
 
-karma_bad = ["-", "не согласен"]
+karma_bad = /^\/(-|Я не согласен|против)/i
 karma_minus = -5
